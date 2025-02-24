66962cc8572c8f56e3de964327358af24a871aac
cdquispe77 <cdq2108@gmail.com>
2025-02-24 08:37:09 -0400

    Update p2python.py

--- Practica 2/p2python.py
+++ Practica 2/p2python.py
@@ -1,20 +1,4 @@
-import math
-
-class Punto:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def coord_cartesianas(self):
-        return (self.x, self.y)
-
-    def coord_polares(self):
-        r = math.sqrt(self.x**2 + self.y**2)
-        theta = math.atan2(self.y, self.x)
-        return (r, theta)
-
-    def __str__(self):
-        return f"Punto(x={self.x}, y={self.y})"
+import matplotlib.pyplot as plt
 
 class Linea:
     def __init__(self, p1, p2):
@@ -25,7 +9,7 @@ class Linea:
         return f"Linea(p1={self.p1}, p2={self.p2})"
 
     def dibujalinea(self):
-        print(f"Dibujando línea desde {self.p1} hasta {self.p2}")
+        plt.plot([self.p1[0], self.p2[0]], [self.p1[1], self.p2[1]], marker='o')
 
 class Circulo:
     def __init__(self, centro, radio):
@@ -36,15 +20,23 @@ class Circulo:
         return f"Circulo(centro={self.centro}, radio={self.radio})"
 
     def dibujacirculo(self):
-        print(f"Dibujando círculo con centro en {self.centro} y radio {self.radio}")
+        circle = plt.Circle((self.centro[0], self.centro[1]), self.radio, color='blue', fill=False)
+        plt.gca().add_patch(circle)
 
-p1 = Punto(1, 2)
-p2 = Punto(3, 4)
+p1 = (1, 2)
+p2 = (3, 4)
+centro = (0, 0)
+
 linea = Linea(p1, p2)
 print(linea)
 linea.dibujalinea()
 
-centro = Punto(0, 0)
 circulo = Circulo(centro, 5)
 print(circulo)
 circulo.dibujacirculo()
+
+plt.xlim(-10, 10)
+plt.ylim(-10, 10)
+plt.gca().set_aspect('equal', adjustable='box')
+plt.grid(True)
+plt.show()
